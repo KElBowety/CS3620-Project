0a84af0650943d06f8e67ec31aba7d429d445ee3
Omar Eltouny <60698807+OmarEltouny78@users.noreply.github.com>
2021-02-26 18:29:57 +0200

    bar chart

--- index.php
+++ index.php
@@ -3,19 +3,62 @@ include "Subject.php";
 include "GoodFood.php";
 include "BadFood.php";
 
-
 $subject = new Subject();
 
-$o1 = new GoodFood ();
+$o1 = new GoodFood();
 $subject->attach($o1);
 
 $o2 = new BadFood();
 $subject->attach($o2);
 
-$subject->someBusinessLogic("21/2/2010");
-$subject->someBusinessLogic("21/2/2021");
+$bar1=$subject->someBusinessLogic("21/2/2010");
+echo $bar1;
+$bar2=$subject->someBusinessLogic("31/4/2011");
 
 $subject->detach($o2);
 
-$subject->someBusinessLogic("21/2/2011");
+$bar3=$subject->someBusinessLogic("2/2/2019");
+
+$bar1=11;
+
+$dataPoints = array(
+    array("y" => 11,"label" => "Food A" ),
+    array("y" =>10,"label" => "Food B" ),
+    array("y" => 2,"label" => "Food C" )
+);
+
 ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <script>
+        window.onload = function() {
+
+            var chart = new CanvasJS.Chart("chartContainer", {
+                animationEnabled: true,
+                title:{
+                    text: "Years left"
+                },
+                axisY: {
+                    title: "years",
+                    includeZero: true,
+                },
+                data: [{
+                    type: "bar",
+                    indexLabel: "{y}",
+                    indexLabelPlacement: "inside",
+                    indexLabelFontWeight: "bolder",
+                    indexLabelFontColor: "white",
+                    dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+                }]
+            });
+            chart.render();
+
+        }
+    </script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>
