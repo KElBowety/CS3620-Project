9dd59433d1fd9cfa3895fb4812f6e7940c83b391
abdelrahmanghanam <abdoghanam@gmail.com>
2021-02-27 15:21:56 +0200

    using save to file temporarly instead of sms

--- EmailObservers.php
+++ EmailObservers.php
@@ -11,24 +11,11 @@ class SMSConfirmer implements SplObserver
 {
     public function update(SplSubject $subject)
     {
-//        echo __METHOD__ . " Emailing the author of post id: " . $subject->userName . " that someone commented with : " . $subject->password . "\n";
 
-        $client = new ClxXmsClient('2c110dcf48fc4654a9ab546ec3b50b46', 'ef24e984606d42ac871eddd5448d18f1');
-
-        $batchParams = new ClxXmsApiMtBatchTextSmsCreate();
-        $batchParams->setSender('447537404817');
-        $batchParams->setRecipients(['201015224979']);
-        $batchParams->setBody("A new Account was created with user name: ".$subject->userName." and password: ".$subject->password);
-
-        try {
-            $result = $client->createTextBatch($batchParams);
-            echo('Successfully sent batch ' . $result->getBatchId());
-        } catch (ClxXmsApiException $ex) {
-            echo('Failed to communicate with XMS: ' . $ex->getMessage() . "
-");
-        }
-
-
+        $filename="myfile.txt";
+        $content="A new Account was created with user name: ".$subject->userName." and password: ".$subject->password;
+        // this one will be replaced with sms code :D
+        file_put_contents($filename, $content);
     }
 }
 class EmailConfirmer implements SplObserver
