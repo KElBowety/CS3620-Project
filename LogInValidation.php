bb030de0eeebb75990c2a1d57bcd878d24c30dbd
Omar Eltouny <60698807+OmarEltouny78@users.noreply.github.com>
2021-03-07 00:34:00 +0200

    Added Validation

---
+++ LogInValidation.php
@@ -0,0 +1,22 @@
+<?php
+require_once('User.php');
+session_start();
+
+
+if (isset($_POST['userName']) && isset($_POST['password'])) {
+
+    $u = new User();
+    $u->setUserName($_POST['userName']);
+    $u->setPassword($_POST['password']);
+    $logResult = $u->logIn();
+    if ($logResult) {
+        $_SESSION["LoginUser"] = $u;
+        $_SESSION["successMessage"] = "تم تسجيل الدخول بنجاح";
+        header("Location: ./AdminPage.php");
+        exit();
+    } else {
+        $_SESSION["errorMessage"] = "خطأ فى إسم المستخدم أو كلمة السر";
+        header("Location: ./LoginPage.php");
+        exit();
+    }
+}
