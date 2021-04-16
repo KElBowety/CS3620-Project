2ee0b62f54c8d5158eb38b9124cf720617e7377b
abdelrahmanghanam <abdoghanam@gmail.com>
2021-04-16 15:00:09 +0200

    fixing user class and donationCRUD

--- FinancialDonationValidation.php
+++ FinancialDonationValidation.php
@@ -11,6 +11,7 @@ if(!isset($_SESSION['donations'])) {
 if(isset($_POST)) {
     if (isset($_POST['value'])){
         $financial= new Financial();
+        $financial->setCurrency("EGP");
         if (!$financial->setAmount($_POST['value'])){
             $_SESSION['errorMessage'] = "قيمة غير صالحة";
             header("Location: ./addDonationPage.php");
